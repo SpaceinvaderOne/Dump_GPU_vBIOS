c6f6e08838fd827996ecd5875a3574710a722c87
SpaceinvaderOne <54743094+SpaceinvaderOne@users.noreply.github.com>
2020-12-22 10:29:34 +0000

    Update address build.sh

--- address build.sh
+++ address build.sh
@@ -1,19 +1,7 @@
-
-
-gpuid="0e:00.0"
-dumpid="0000:$gpuid"
-
-
-
-
-
-build1=$(echo "$dumpid" | sed "s?0000?<address domain='0x0000'?") 
-build2=$(echo "$build1" | sed "s?:? bus='0x?") 
-build3=$(echo "$build2" | sed "s?:?' slot='0x?")
-build4=$(echo "$build3" | sed "s?\.?' function='0x?")
-address=$(echo "$build4" | sed  "s?.*?&'/>?") 
-echo $address
-
-
-
+#!/bin/bash
+#test to convert id to xml address (not working in userscripts)
+id="0000:0e:00.0"
+addressid=$(echo "<address domain='0x${id:0:4}' bus='0x${id:5:2}' slot='0x${id:8:2}' function='0x${id:11:1}'/>")
+echo $addressid
+exit
 
